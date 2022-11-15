bdd6f3eb801f3e2049609a00ecdce802061f7e36
dmtrbskkv <baskakovdima99@mail.ru>
2022-11-15 15:44:02 +1000

    Заполнение мета полей WP и iiko пользователя

--- cms/wordpress/endpoints.php
+++ cms/wordpress/endpoints.php
@@ -29,7 +29,10 @@ function tinkoff_auth_callback( WP_REST_Request $request ) {
 	$password    = md5( time() . rand( 0, 100 ) . rand( 0, 200 ) );
 
 	if ( ! $email || ! $username ) {
-		$response->header( 'Location', tinkoff_auth_helper_format_redirect_url( false, 'Предоставленных данных недостаточно' ) );
+		$response->header(
+			'Location',
+			tinkoff_auth_helper_format_redirect_url( false, 'Предоставленных данных недостаточно' )
+		);
 
 		return $response;
 	}
@@ -38,25 +41,52 @@ function tinkoff_auth_callback( WP_REST_Request $request ) {
 	if ( $user !== false ) {
 		$is_tinkoff_user = $user->get( 'is_tinkoff' );
 		if ( ! $is_tinkoff_user ) {
-			$response->header( 'Location', tinkoff_auth_helper_format_redirect_url( false, 'Пользователь с такой почтой уже существует' ) );
+			$response->header(
+				'Location',
+				tinkoff_auth_helper_format_redirect_url( false, 'Пользователь с такой почтой уже существует' )
+			);
 
 			return $response;
 		}
 
 		$user_id = $user->get( 'id' );
 	} else {
-		$user_id = wp_create_user( $username, $password, $email );
-		if ( is_wp_error( $user_id ) ) {
-			$response->header( 'Location', tinkoff_auth_helper_format_redirect_url( false, 'Ошибка при создании пользователя' ) );
+		$user_id = null;
+		if ( function_exists( 'wc_create_new_customer' ) ) {
+			$user_id = wc_create_new_customer( $username, $password, $email );
+		}
+
+		if ( is_null( $user_id ) ) {
+			$user_id = wp_create_user( $username, $password, $email );
+		}
+
+		if ( ! $user_id || is_wp_error( $user_id ) ) {
+			$response->header(
+				'Location',
+				tinkoff_auth_helper_format_redirect_url( false, 'Ошибка при создании пользователя' )
+			);
 
 			return $response;
 		}
 	}
 
-
+	// Профиль WP
 	update_user_meta( $user_id, 'is_tinkoff', true );
 	update_user_meta( $user_id, 'first_name', $credentials['given_name'] ?? '' );
 	update_user_meta( $user_id, 'last_name', $credentials['family_name'] ?? '' );
+
+	// Плагин iiko
+	update_user_meta( $user_id, 'iiko_email', $credentials['email'] ?? '' );
+	update_user_meta( $user_id, 'iiko_name', $credentials['given_name'] ?? '' );
+	update_user_meta( $user_id, 'iiko_middleName', $credentials['middle_name'] ?? '' );
+	update_user_meta( $user_id, 'iiko_middleName', $credentials['middle_name'] ?? '' );
+	update_user_meta( $user_id, 'iiko_surName', $credentials['family_name'] ?? '' );
+	update_user_meta( $user_id, 'iiko_phone', $username );
+
+	// Билинг
+	update_user_meta( $user_id, 'billing_first_name', $credentials['given_name'] ?? '' );
+	update_user_meta( $user_id, 'billing_phone', $username );
+
 	wp_set_auth_cookie( $user_id );
 
 	$response->header( 'Location', tinkoff_auth_helper_format_redirect_url() );
