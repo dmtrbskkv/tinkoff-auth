ed8746602ea0c032c8425b859369da220bc34248
dmtrbskkv <baskakovdima99@mail.ru>
2022-11-21 22:48:37 +1000

    Добавление провайдера сессии для хранения state

--- src/Services/State/Providers/Session.php
+++ src/Services/State/Providers/Session.php
@@ -2,7 +2,29 @@
 
 namespace TinkoffAuth\Services\State\Providers;
 
-class Session
+class Session extends Provider
 {
+    const STATE_NAME = 'tinkoff_auth_state';
+
+    public function createState(): bool
+    {
+        session_start();
+
+        $this->state = bin2hex(random_bytes(20));
+
+        $_SESSION[self::STATE_NAME] = $this->state;
+
+        return true;
+    }
+
+    public function validateState(string $string = null): bool
+    {
+        $sessionState = $_SESSION[self::STATE_NAME] ?? null;
+        if (is_null($sessionState)) {
+            return false;
+        }
+
+        return $sessionState === $string;
+    }
 
 }
