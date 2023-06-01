2e60f641e195792cafccf876b25a232b55b38b89
dmitry <baskakovdima99@mail.ru>
2023-06-01 20:37:52 +0300

    Рефакторинг кода

--- cms/wordpress/extensions/autoload.php
+++ cms/wordpress/extensions/autoload.php
@@ -1,13 +1,13 @@
 <?php
 
-$ext = scandir(__DIR__);
+$ext = scandir( __DIR__ );
 
-$exceptions = ['.gitkeep', 'autoload.php'];
-for ($i = 2; $i < count($ext); $i++) {
-    $filename = $ext[$i];
-    if (in_array($filename, $exceptions)) {
-        continue;
-    }
+$exceptions = [ '.gitkeep', 'autoload.php' ];
+for ( $i = 2; $i < count( $ext ); $i ++ ) {
+	$filename = $ext[ $i ];
+	if ( in_array( $filename, $exceptions ) ) {
+		continue;
+	}
 
-    require_once __DIR__ . "/$filename";
+	require_once __DIR__ . "/$filename";
 }
