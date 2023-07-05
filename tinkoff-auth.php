26b40a28bb1bf6222723135f5720502c3d23608b
Dmitry Baskakov <baskakovdima99@mail.ru>
2023-07-05 21:34:58 +1000

    Update tinkoff-auth.php

--- tinkoff-auth.php
+++ tinkoff-auth.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.tinkoff.ru/tinkoff-id/
 Description: Плагин авторизации через Тинькофф ID
 Version: 0.6
 Author: Tinkoff ID Team
-Author URI: https://www.tinkoff.ru/tinkoff-id/п
+Author URI: https://www.tinkoff.ru/tinkoff-id/
 */
 
 require_once __DIR__ . '/cms/wordpress.php';
